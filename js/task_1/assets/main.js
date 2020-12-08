0ef76584a8a8e1923911f6f146f572fd80c91b90
pushkining <pushkining@ua.fm>
2020-12-08 11:31:52 +0200

    raw#5

---
+++ js/task_1/assets/main.js
@@ -0,0 +1,28 @@
+let a = prompt('введите число');
+//let r = prompt('введите число еще раз');
+
+// let b = a*4;
+// console.log(`Периметр кадрата: ${b}`);
+
+// let b = a**3;
+// let c = a**2;
+// console.log(`Объем куба: ${b}`);
+// console.log(`Площадь стороны куба: ${c}`);
+
+// const pi = 3.1415;
+// let b = 2*pi*a;
+// let c = pi*a**2;
+// console.log(`Длина окружности: ${b}`);
+// console.log(`Площадь круга: ${c}`);
+
+// let b = a/r;
+// console.log(`Плотность вещества: ${b} г/см3`);
+
+// let b = a/r;
+// console.log(`Плотность населения: ${b} чел/км2`);
+
+// let b = Math.sqrt(a**2 + r**2);
+// console.log(`Длина гипотенузы: ${b}`);
+
+let b = (a**2 + 10) / Math.sqrt(a**2 + 1)
+console.log(`y = ${b}`);
