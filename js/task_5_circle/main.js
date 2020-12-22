31e37494e2ff70a9291a21e90080cb1662c30ad7
pushkining <pushkining@ua.fm>
2020-12-22 15:10:34 +0200

    v1.3

--- js/task_5_circle/main.js
+++ js/task_5_circle/main.js
@@ -1,30 +1,24 @@
 
-for (let i = 2; i <= 1000; i++) {
-    for (let j = 2; j < i; j++) {
-        if (i % j == 0) {
-            break;
-        }
-        else if (i == j + 1) {
-            console.log(i);
-        }
-    }
-}
+// for (let i = 2; i <= 1000; i++) {
+//     for (let j = 2; j < i; j++) {
+//         if (i % j == 0) {
+//             break;
+//         }
+//         else if (i == j + 1) {
+//             console.log(i);
+//         }
+//     }
+// }
 
 //=============
-//  let limit = 1000;
-//  let list = [];
-// for (let j = 2; j <= limit;  j++) list[j] = j;
- 
-// let P = 2;
-// while (P * P <= limit)
-//     {
-//     for (let j = P + P; j <= limit; j = j + P) delete list[j];
-//     for (let j = P + 1; j <= limit; j++) if (list[j]) {P = j; break;}
-//     }
 
-// //for ( j = 0; j <= limit; j++) if (list[j]) S += list[j];
-// // for (const item of list) {
-// //     console.log(item);
-// // }
-// console.log (list);
-
+console.log(3);
+console.log(5);
+console.log(7);  
+for (let i = 2; i <=1000; i++) {
+    if (i % 2 == 0) continue;
+    if (i % 3 == 0) continue;
+    if (i % 5 == 0) continue;
+    if (i % 7 == 0) continue;
+    console.log(i);
+}
